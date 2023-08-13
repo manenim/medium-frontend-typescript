82c70367926059caad2971f9908512469d65dc1d
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-08-13 08:30:36 +0100

    implemented passport-local

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 
+import { useEffect } from 'react'
 import './App.css'
 import Login from './pages/Login'
+import axios from 'axios'
+
 
 function App() {
   // const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+        const authStatus = await axios.get("http://localhost:3000/auth/status", {withCredentials: true})
+        console.log(authStatus)
+
+    }
+    return () => checkAuthStatus()
+  
+  
+  }, [])
+  
 
   return (
     <>
